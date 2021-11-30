16baaab91b44ebbd6f6cef189140d5edb4682b42
Evan Supple <esupple@wgu.edu>
2021-11-30 15:55:31 -0500

    add email fields to roles

--- mysite/lion4/forms.py
+++ mysite/lion4/forms.py
@@ -11,9 +11,11 @@ class InputForm(forms.Form):
     buyer=forms.CharField(label='Buyer', max_length=100, required=False)
     buyer_firm=forms.CharField(label='Buyers Firm', max_length=100, required=False)
     buyer_phone=forms.CharField(label='Buyer Phone', max_length=100, required=False)
+    buyer_email=forms.CharField(label='Buyer Email', max_length=100, required=False)
     seller=forms.CharField(label='Seller', max_length=100, required=False)
     seller_firm= forms.CharField(label='Seller Firm', max_length=100, required=False)
     seller_phone=forms.CharField(label='Seller Phone', max_length=100, required=False)
+    seller_email=forms.CharField(label='Seller Email', max_length=100, required=False)
     property=forms.CharField(label='Property Address', max_length=100, required=False)
     lot_no=forms.CharField(label='Lot No.', max_length=100, required=False)
     purchase=forms.CharField(label='Purchase Price', max_length=100, required=False)
@@ -21,12 +23,15 @@ class InputForm(forms.Form):
     attorney=forms.CharField(label='Attorney', max_length=100, required=False)
     attorney_firm=forms.CharField(label='Attorney Firm', max_length=100, required=False)
     attorney_phone=forms.CharField(label='Attorney Phone', max_length=100, required=False)
+    attorney_email=forms.CharField(label='Attorney Email', max_length=100, required=False)
     lender=forms.CharField(label='Lender', max_length=100, required=False)
     loan_office=forms.CharField(label='Loan Office', max_length=100, required=False)
     lender_phone=forms.CharField(label='Lender Phone', max_length=100, required=False)
+    lender_email=forms.CharField(label='Lender Email', max_length=100, required=False)
     broker=forms.CharField(label='Broker', max_length=100, required=False)
     broker_firm=forms.CharField(label='Broker Firm', max_length=100, required=False)
     broker_phone=forms.CharField(label='Broker Phone', max_length=100, required=False)
+    broker_email=forms.CharField(label='Broker Email', max_length=100, required=False)
 
 
 
--- mysite/lion4/models.py
+++ mysite/lion4/models.py
@@ -42,9 +42,11 @@ class Standard_Record(models.Model):
     buyer = models.TextField(blank=True, default = my_default)
     buyer_firm = models.TextField(blank=True, default = my_default)
     buyer_phone = models.TextField(blank=True, default = my_default)
+    buyer_email= models.TextField(blank=True, default = my_default)
     seller = models.TextField(blank=True, default = my_default)
     seller_firm = models.TextField(blank=True, default = my_default)
     seller_phone = models.TextField(blank=True, default = my_default)
+    seller_email= models.TextField(blank=True, default = my_default)
     client = models.TextField(blank=True, default = my_default)
     property = models.TextField(blank=True, default = my_default)
     lot_no = models.TextField(blank=True, default = my_default)
@@ -53,12 +55,15 @@ class Standard_Record(models.Model):
     attorney = models.TextField(blank=True, default = my_default)
     attorney_firm = models.TextField(blank=True, default = my_default)
     attorney_phone = models.TextField(blank=True, default = my_default)
+    attorney_email= models.TextField(blank=True, default = my_default)
     lender = models.TextField(blank=True, default = my_default)
     loan_office = models.TextField(blank=True, default = my_default)
     lender_phone = models.TextField(blank=True, default = my_default)
+    lender_email= models.TextField(blank=True, default = my_default)
     broker = models.TextField(blank=True, default = my_default)
     broker_firm = models.TextField(blank=True, default = my_default)
     broker_phone = models.TextField(blank=True, default = my_default)
+    broker_email= models.TextField(blank=True, default = my_default)
 
 
 
